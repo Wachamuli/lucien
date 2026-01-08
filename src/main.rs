8440acb90c22c127289c6505b77c032a8e3d25f5
Wachamuli <josemrr27@gmail.com>
2026-01-08 16:40:58 -0600

    tracing (subscriber and appender) are ready to go

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,39 @@ use iced_layershell::{
     reexport::{Anchor, KeyboardInteractivity, Layer},
     settings::LayerShellSettings,
 };
+use tracing::Level;
+use tracing_subscriber::{EnvFilter, fmt, prelude::*};
 
 pub fn main() -> iced_layershell::Result {
+    std::panic::set_hook(Box::new(|panic_info| {
+        eprintln!("LAUNCHER CRASHED: {}", panic_info);
+    }));
+
+    let package_name = env!("CARGO_PKG_NAME");
+    let xdg_dirs = xdg::BaseDirectories::with_prefix(&package_name);
+    let cache_dir = xdg_dirs.get_cache_home().expect(
+        "Could not determine the user's Home directory. Ensure the $HOME environment variable is set."
+    );
+    let file_appender = tracing_appender::rolling::daily(cache_dir, "logs");
+    let (non_blocking_file, _guard) = tracing_appender::non_blocking(file_appender);
+    let env_filter = EnvFilter::from_default_env()
+        .add_directive(Level::INFO.into())
+        .add_directive("iced_wgpu=error".parse().unwrap())
+        .add_directive("usvg=error".parse().unwrap())
+        .add_directive("wgpu_hal=error".parse().unwrap())
+        .add_directive("wgpu_core=error".parse().unwrap())
+        .add_directive("wgpu_core=error".parse().unwrap())
+        .add_directive(format!("{package_name}=info").parse().unwrap());
+
+    tracing_subscriber::registry()
+        .with(env_filter)
+        .with(fmt::layer().with_writer(std::io::stdout))
+        .with(fmt::layer().with_ansi(false).with_writer(non_blocking_file))
+        .init();
+
+    let package_version = env!("CARGO_PKG_VERSION");
+    tracing::info!("Running {package_name} v. {package_version}...");
+
     let layershell_settings = LayerShellSettings {
         size: Some((700, 500)),
         anchor: Anchor::Bottom | Anchor::Left | Anchor::Right | Anchor::Top,
