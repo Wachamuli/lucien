2790a523602018728c6bd2c87be6f97e58e934c9
Wachamuli <josemrr27@gmail.com>
2026-02-26 10:23:24 -0600

    _ prefix removed from appender guard

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,7 @@ fn setup_tracing_subscriber(
     filename: &str,
 ) -> anyhow::Result<tracing_appender::non_blocking::WorkerGuard> {
     let file_appender = tracing_appender::rolling::daily(cache_dir, filename);
-    let (non_blocking_file, _logger_guard) = tracing_appender::non_blocking(file_appender);
+    let (non_blocking_file, guard) = tracing_appender::non_blocking(file_appender);
     let env_filter = EnvFilter::from_default_env()
         .add_directive(Level::INFO.into())
         .add_directive("iced_wgpu=error".parse()?)
@@ -76,7 +76,7 @@ fn setup_tracing_subscriber(
         .with(fmt::layer().with_ansi(false).with_writer(non_blocking_file))
         .init();
 
-    Ok(_logger_guard)
+    Ok(guard)
 }
 
 fn get_single_instance(name: &str) -> anyhow::Result<OwnedFd> {
