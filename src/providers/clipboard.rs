ef013c04a73f9e8659eb09860577f51139c3f582
Wachamuli <josemrr27@gmail.com>
2026-03-14 19:35:23 -0500

    clipboard ordered by recency

--- src/providers/clipboard.rs
+++ src/providers/clipboard.rs
@@ -37,22 +37,11 @@ impl Provider for ClipboardProvider {
                 .await
                 .unwrap();
 
-            sqlx::query(
-                r#"
-                CREATE TABLE IF NOT EXISTS entries (
-                    id TEXT PRIMARY KEY,
-                    main TEXT NOT NULL,
-                    secondary TEXT,
-                    provider TEXT NOT NULL,
-                    icon BLOB
-                )
-                "#,
-            )
-            .execute(&mut conn)
-            .await
-            .unwrap();
+            migrate(&mut conn).await;
 
-            let mut entries = sqlx::query_as::<_, Entry>("SELECT * FROM entries").fetch(&mut conn);
+            let mut entries =
+                sqlx::query_as::<_, Entry>("SELECT * FROM entries ORDER BY created_at DESC")
+                    .fetch(&mut conn);
 
             while let Some(Ok(entry)) = entries.next().await {
                 scanner.load(entry).await;
@@ -86,6 +75,41 @@ impl Provider for ClipboardProvider {
         window::latest().and_then(window::close)
     }
 }
+async fn migrate(conn: &mut sqlx::SqliteConnection) -> Result<(), sqlx::Error> {
+    let mut tx = conn.begin().await?;
+    sqlx::query(
+        r#"
+        CREATE TABLE IF NOT EXISTS entries (
+            id TEXT PRIMARY KEY,
+            main TEXT NOT NULL,
+            secondary TEXT,
+            icon BLOB,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+        "#,
+    )
+    .execute(&mut *tx)
+    .await?;
+    sqlx::query(
+        r#"
+        CREATE TRIGGER IF NOT EXISTS limit_entries_to_20
+        AFTER INSERT ON entries
+        BEGIN
+            DELETE FROM entries
+            WHERE id NOT IN (
+                SELECT id FROM entries
+                ORDER BY created_at DESC
+                LIMIT 20
+            );
+        END;
+        "#,
+    )
+    .execute(&mut *tx)
+    .await?;
+
+    tx.commit().await?;
+    Ok(())
+}
 
 pub async fn handle_clipboard_insertion(content: &str, package_name: &str) -> anyhow::Result<()> {
     let clipboard_dir = xdg::BaseDirectories::with_prefix(package_name)
@@ -99,26 +123,15 @@ pub async fn handle_clipboard_insertion(content: &str, package_name: &str) -> an
     let mut conn = sqlx::SqliteConnection::connect_with(&conn_options)
         .await
         .unwrap();
-    let mut transaction = conn.begin().await?;
-    sqlx::query(
-        r#"
-        CREATE TABLE IF NOT EXISTS entries (
-                    id TEXT PRIMARY KEY,
-                    main TEXT NOT NULL,
-                    secondary TEXT,
-                    icon BLOB
-                )
-        "#,
-    )
-    .execute(&mut *transaction)
-    .await?;
+
+    migrate(&mut conn).await;
+
     sqlx::query("INSERT OR IGNORE INTO entries (id, main, secondary, icon) VALUES (?, ?, ?, NULL)")
         .bind(content)
         .bind(content)
         .bind("Text")
-        .execute(&mut *transaction)
+        .execute(&mut conn)
         .await?;
 
-    transaction.commit().await?;
     Ok(())
 }
